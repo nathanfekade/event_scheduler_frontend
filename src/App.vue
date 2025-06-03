e2d9a581cd44c3f59d7791a6578b4c2fc099e06c
Nathan Fekade <nathanfekade276@gmail.com>
2025-06-03 19:37:59 +0300

    style: Updated global styles and styles in app.vue

--- src/App.vue
+++ src/App.vue
@@ -28,47 +28,60 @@ const handleLogout = () => {
 </template>
 
 <style scoped>
-h1{
+.nav {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  padding: 1rem;
+  background-color: #1a1a1a;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.3);
+  z-index: 1000;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+h1 {
   color: #FDFCFE;
-  margin-left: 40px;
-  text-align: left;
+  margin: 0;
+  font-size: 1.8rem;
 }
 
 .login {
-    background-color: #8965A3;
-    width: 90px;
-    height: 40px;
-    border-radius: 50px;
-    margin-top: -62px;
-    margin-right: 25px;
-    float: right;
-    color: #FDFCFE;
-    font-size: 1.3rem;
-    border: none;
-    text-decoration: none;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-
-}
-
-.username {
-  
-  margin-top: -62px;
-  margin-right: 25px;
-  float: right;
-  color: #FDFCFE;
-  font-size: 1.3rem;
-}
-
-.logout {
-  background-color: #8965A3;
+  background-color: #6b46c1;
   width: 90px;
   height: 40px;
   border-radius: 50px;
   color: #FDFCFE;
   font-size: 1.3rem;
   border: none;
-  margin-left: 10px;
+  text-decoration: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.username {
+  color: #FDFCFE;
+  font-size: 1.3rem;
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+}
+
+.logout {
+  background-color: #6b46c1;
+  width: 90px;
+  height: 40px;
+  border-radius: 50px;
+  color: #FDFCFE;
+  font-size: 1.3rem;
+  border: none;
+  cursor: pointer;
+}
+
+.login:hover, .logout:hover {
+  background-color: #553c9a;
 }
 </style>
